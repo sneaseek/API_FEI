ab04125bdbcc64c4c70e820ef1a3dd42c6395439
sneaseek <37507250+sneaseek@users.noreply.github.com>
2018-04-06 17:30:14 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
-#include <float.h>
 
-int main(void) {
-
-    double strojove_epsilon_double = 1.0;
-    double temp_double = 1.0 + strojove_epsilon_double;
-    float strojove_epsilon_float = 1;
-    float temp_float = 1 + strojove_epsilon_float;
-
-    while (temp_float > 1) {
-        strojove_epsilon_float = strojove_epsilon_float / 2;
-        temp_float = 1 + strojove_epsilon_float;
+int main() {
+    int r,i,n,space,ncr;
+    printf("Zvolte pocet riadkov\n");
+    scanf("%d",&n);
+    for(i = 0;i < n; i++){
+        for(space = 1; space < n-i; space++)
+            printf(" ");
+        for(r = 0; r <= i; r++){
+            if(n==0 || r==0){
+                ncr = 1;
+                printf("%d ", ncr);
+            }
+            else{
+                ncr = ncr*(i - r + 1) / r;
+                if (ncr % 2 == 1)
+                    printf("%d ",ncr);
+                if (ncr % 2 == 0)
+                    printf("* ");
+            }
+        }
+        printf("\n");
     }
-    printf("Strojove epsilon pre float je - %.17g\n", strojove_epsilon_float * 2);
-    printf("Najnizsie cislo float nezaokruhlene na 1 - %.17g\n", 1 + (strojove_epsilon_float * 2));
-    printf("Hodnota FLT_EPSILON je - %.17g\n\n", FLT_EPSILON);
-
-    while (temp_double > 1.0) {
-        strojove_epsilon_double /= 2.0;
-        temp_double = 1.0 + strojove_epsilon_double;
-    }
-    printf("Strojove epsilon pre float je - %.17g\n",strojove_epsilon_double * 2.0);
-    printf("Najnizsie cislo double nezaokruhlene na 1 - %.17g\n", 1.0 + (strojove_epsilon_double * 2.0));
-    printf("Hodnota DBL_EPSILON je - %.17g\n", DBL_EPSILON);
     return 0;
 }
